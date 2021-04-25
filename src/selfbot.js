f73867d33bcd2176a8ff0876494bb0531e6b259b
ItsKek <proverkaakksteam@mail.ru>
2021-04-25 18:00:06 +1000

    Обновление лога консоли

--- src/selfbot.js
+++ src/selfbot.js
@@ -5,16 +5,20 @@ const client = new Client();
 
 client.login(config.token);
 console.log("Успешный вход.. Получаю информацию из конфига")
+console.log("=============================================")
 console.log("Твой дискорд токен:", config.token)
+console.log("=============================================")
 client.on('ready', () => {
     config.commands.forEach(command => {
         const channel = client.channels.get(command.channelID);
         if(!channel || !command.active) return;
+        console.log("Параметры получены, запуск служб....")
         setInterval(() => {
             try {
                 if(command.inventory.say.count > 0 && command.inventory.say.active) {
                     channel.send(command.inventory.say.content).catch(() => {});
                     console.log("\x1b[36m%s\x1b[0m", "Сообщение отправлено.")
+                    console.log("Отправка сообщения....")
                     command.inventory.say.count--
                 } else if(command.inventory.say1.count > 0 && command.inventory.say1.active) {
                     channel.send(command.inventory.say1.content).catch(() => {});
